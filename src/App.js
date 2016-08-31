4307d1e002aba34d2ee33187cb6a6a4e175f7ab1
Federico Marcos <marcosfede@gmail.com>
2016-08-31 02:35:55 -0300

    ajax

--- src/App.js
+++ src/App.js
@@ -1,21 +1,36 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
+import $ from 'jquery'
 
 class App extends Component {
-  render() {
+  constructor () {
+    super()
+    this.state = {
+      quote: 'Push the button to generate a new quote'
+    }
+  }
+
+  newQuote = () => {
+    let quoteList = [ 'quote1', 'quote2', 'quote3' ]
+    this.setState({ quote: quoteList[ Math.floor(Math.random() * quoteList.length) ] })
+  }
+  newQuoteJson = () => {
+    $.getJSON("http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1&callback=", (a) => {
+      this.setState({ quote: a[0].content })
+    });
+  }
+
+  render () {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+      <div className='App'>
+        <div id='quote'>{this.state.quote}</div>
+        <div id='buttons'>
+          <button id='newQuote' onClick={this.newQuote}>new</button>
+          <button id='shareQuote' onClick={this.newQuoteJson}>share</button>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
